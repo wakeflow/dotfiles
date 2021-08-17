c854b346c90a1b3da241faa0bed3a5976767d7e3
Andreas <andi@wakeflow.io>
2021-08-17 20:18:08 +0200

    gbo

--- scripts/gbo.js
+++ scripts/gbo.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-const { runCommands } = require("./runCommands");
+const { runCommands } = require(`./runCommands`)
 
 runCommands([
-  'git fetch',
-  'git checkout master',
-  'git pull origin master',
+  `git fetch`,
+  `git checkout master`,
+  `git pull origin master`,
   `git checkout -b ${process.argv[2]}`,
   `git push --set-upstream origin ${process.argv[2]}`,
 ])
