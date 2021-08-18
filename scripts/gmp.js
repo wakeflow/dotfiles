49605c58b74604ad9d29713ba9d2b4fac3b3c754
Andreas <andi@wakeflow.io>
2021-08-18 11:41:14 +0200

    asdf

--- scripts/gmp.js
+++ scripts/gmp.js
@@ -7,6 +7,7 @@ import { run,runCommands } from './runCommands.js'
 const response = await run(`git branch`)
 const branch = response.slice(2).trim()
 if(branch === `master`) throw new Error(`You are on master branch`)
+console.log({ branch })
 runCommands([
   `git fetch origin -q`,
   `git rebase origin/master -q`,
