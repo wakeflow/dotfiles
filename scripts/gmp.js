9ba2a8e095a2dedca576bc9c50a2a45694cf3466
Andreas <andi@wakeflow.io>
2021-08-18 11:47:55 +0200

    gmp

--- scripts/gmp.js
+++ scripts/gmp.js
@@ -2,18 +2,16 @@
 
 //gmpr = git merge pull request
 
-import { run,runCommands } from './runCommands.js'
+import { run } from './runCommands.js'
 
 const response = await run(`git branch`)
 const branch = response.split(`\n`).find(line => line.includes(`*`)).slice(2).trim()
 if(branch === `master`) throw new Error(`You are on master branch`)
-runCommands([
-  `git fetch origin -q`,
-  `git rebase origin/master -q`,
-  `git push -f -q`,
-  `git checkout master -q`,
-  `git merge ${branch} --no-edit --ff-only -m "hello"`,
-  `git push -q`,
-  `git push origin --delete ${branch}`,
-  `git branch -d ${branch}`,
-])
+await run(`git fetch origin -q`)
+await run(`git rebase origin/master -q`)
+await run(`git push -f -q`)
+await run(`git checkout master -q`)
+await run(`git merge ${branch} --no-edit --ff-only -m "hello"`)
+await run(`git push -q`)
+await run(`git push origin --delete ${branch}`,true)
+await run(`git branch -d ${branch}`)
