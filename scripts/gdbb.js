dc13f4eddebbb93b41070f740d8873cd8dd1b6a0
Andreas <andi@wakeflow.io>
2021-08-18 11:32:26 +0200

    quiet on deleteing remote branch in gdbb

--- scripts/gdbb.js
+++ scripts/gdbb.js
@@ -2,11 +2,9 @@
 
 //gdbb = git delete both branches (both local and remote)
 
-import { runCommands } from './runCommands.js'
+import { run } from './runCommands.js'
 
-runCommands([
-  `git checkout master -q`,
-  `git branch -D ${process.argv[2]}`,
-  `git remote prune origin`,
-  `git push --delete origin ${process.argv[2]}`,
-])
+await run(`git checkout master -q`)
+await run(`git branch -D ${process.argv[2]}`)
+await run(`git remote prune origin`)
+await run(`git push --delete origin ${process.argv[2]}`,true)
