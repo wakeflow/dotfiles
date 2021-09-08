c586f70a2964af1abc5d823b8a34e2f3b8805dd3
Andreas <andi@wakeflow.io>
2021-09-08 12:14:00 +0100

    only checking out master if you're on the branch you want to delete

--- scripts/gbo.js
+++ scripts/gbo.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 import { run } from './runCommands.js'
 
+const response = await run(`git branch`,true)
+const branch = response.split(`\n`).find(line => line.includes(`*`)).slice(2).trim()
+
 await run(`git fetch`)
-await run(`git checkout master -q`)
+if(branch === process.argv[2])
+  await run(`git checkout master -q`)
 await run(`git pull origin master -q`)
 await run(`git checkout -b "${process.argv[2]}" -q`)
 await run(`git push --set-upstream origin "${process.argv[2]}"`,true)
